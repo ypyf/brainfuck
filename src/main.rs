6bfd24be7a0b5bfe159e5f38b03d0dbcdafab158
Yan Feng <t34@qq.com>
2023-09-29 02:53:25 +0800

    refactor compiler

--- src/main.rs
+++ src/main.rs
@@ -1,8 +1,6 @@
-use std::cell::RefCell;
 use std::env;
 use std::fs;
 use std::io::{self, Write};
-use std::rc::Rc;
 
 #[derive(Debug)]
 struct Context {
@@ -17,21 +15,132 @@ struct Error {
 
 #[derive(PartialEq, Debug, Clone)]
 enum Command {
-    Inc,                    // +
-    Dec,                    // -
-    IncPtr,                 // >
-    DecPtr,                 // <
-    Read,                   // ,
-    Write,                  // .
-    JZ(Rc<RefCell<usize>>), // [
-    JNZ(usize),             // ]
+    Inc,              // +
+    Dec,              // -
+    IncPtr,           // >
+    DecPtr,           // <
+    Read,             // ,
+    Write,            // .
+    EnterLoop(usize), // [
+    ExitLoop(usize),  // ]
+}
+
+struct Compiler {
+    pub program: Vec<Command>,
+}
+
+impl Compiler {
+    pub fn new() -> Self {
+        Compiler { program: vec![] }
+    }
+
+    pub fn compile(&mut self, source: &str) -> Result<&Vec<Command>, Error> {
+        self.generate_instructions(source)?.calculate_jump_address();
+        Ok(&self.program)
+    }
+
+    fn generate_instructions(&mut self, source: &str) -> Result<&mut Self, Error> {
+        let mut position: usize = 0;
+        self.program = source
+            .chars()
+            .filter_map(|ch| {
+                let command = match ch {
+                    '+' => Some(Command::Inc),
+                    '-' => Some(Command::Dec),
+                    '>' => Some(Command::IncPtr),
+                    '<' => Some(Command::DecPtr),
+                    ',' => Some(Command::Read),
+                    '.' => Some(Command::Write),
+                    '[' => Some(Command::EnterLoop(0)),
+                    ']' => Some(Command::ExitLoop(0)),
+                    _ => None,
+                };
+
+                if command.is_some() {
+                    position += 1
+                }
+
+                command
+            })
+            .collect();
+
+        Ok(self)
+    }
+
+    fn calculate_jump_address(&mut self) -> &mut Self {
+        let mut position: usize = 0;
+        let mut jumps: Vec<&mut Command> = vec![];
+        for command in &mut self.program {
+            match command {
+                Command::EnterLoop(_) => {
+                    *command = Command::ExitLoop(position);
+                    jumps.push(command)
+                }
+                Command::ExitLoop(_) => {
+                    *command = Command::EnterLoop(position);
+                    if let Some(top) = jumps.pop() {
+                        std::mem::swap(top, command)
+                    } else {
+                        todo!()
+                    }
+                }
+                _ => (),
+            }
+            position += 1;
+        }
+        self
+    }
+}
+
+fn execute(ctx: &mut Context, program: &Vec<Command>) {
+    let mut i = 0;
+    while ctx.pc < program.len() {
+        match program[ctx.pc] {
+            Command::Inc => ctx.memory[i] = ctx.memory[i].overflowing_add(1).0,
+            Command::Dec => ctx.memory[i] -= 1,
+            Command::IncPtr => i += 1,
+            Command::DecPtr => i -= 1,
+            Command::Read => {
+                let mut input = String::new();
+                io::stdin().read_line(&mut input).unwrap();
+                ctx.memory[i] = input.as_bytes()[0]
+            }
+            Command::Write => print!("{}", ctx.memory[i] as char),
+            Command::EnterLoop(disp) => {
+                if ctx.memory[i] == 0 {
+                    ctx.pc = disp
+                }
+            }
+            Command::ExitLoop(disp) => {
+                if ctx.memory[i] != 0 {
+                    ctx.pc = disp
+                }
+            }
+        }
+        ctx.pc += 1;
+    }
+}
+
+fn run(source: &str) {
+    let mut ctx = Context {
+        memory: [0; 30000],
+        pc: 0,
+    };
+    let mut compiler = Compiler::new();
+    match compiler.compile(&source) {
+        Ok(program) => {
+            // println!("{:?}", program);
+            execute(&mut ctx, program)
+        }
+        Err(err) => println!("error: {}", err.message),
+    }
 }
 
 fn main() {
     let args: Vec<String> = env::args().collect();
     match args.get(1) {
         Some(filename) => {
-            let input = read_file(filename);
+            let input = fs::read_to_string(filename).unwrap();
             run(&input)
         }
         None => loop {
@@ -44,94 +153,4 @@ fn main() {
             }
         },
     }
-}
-
-fn run(source: &str) {
-    let mut ctx = Context {
-        memory: [0; 30000],
-        pc: 0,
-    };
-    match compile(&source) {
-        Ok(program) => {
-            // println!("{:?}", program);
-            execute(&mut ctx, &program)
-        }
-        Err(err) => println!("error: {}", err.message),
-    }
-}
-
-fn compile(source: &str) -> Result<Vec<Command>, Error> {
-    let mut position: usize = 0;
-    let mut labels: Vec<Rc<RefCell<usize>>> = vec![];
-    let mut program: Vec<Command> = vec![];
-    for ch in source.chars() {
-        match ch {
-            '+' => program.push(Command::Inc),
-            '-' => program.push(Command::Dec),
-            '>' => program.push(Command::IncPtr),
-            '<' => program.push(Command::DecPtr),
-            ',' => program.push(Command::Read),
-            '.' => program.push(Command::Write),
-            '[' => {
-                let label = Rc::new(RefCell::new(position));
-                let command = Command::JZ(label.clone());
-                labels.push(label);
-                program.push(command)
-            }
-            ']' => {
-                if let Some(label) = labels.pop() {
-                    let disp = position - *label.borrow();
-                    *label.borrow_mut() = disp;
-                    program.push(Command::JNZ(disp))
-                } else {
-                    return Err(Error {
-                        message: "too many close brackets",
-                    });
-                }
-            }
-            _ => continue,
-        }
-        position += 1;
-    }
-
-    if !labels.is_empty() {
-        return Err(Error {
-            message: "too many open brackets",
-        });
-    }
-
-    Ok(program)
-}
-
-fn execute(ctx: &mut Context, program: &Vec<Command>) {
-    let mut i = 0;
-    while ctx.pc < program.len() {
-        match &program[ctx.pc] {
-            Command::Inc => ctx.memory[i] = ctx.memory[i].overflowing_add(1).0,
-            Command::Dec => ctx.memory[i] -= 1,
-            Command::IncPtr => i += 1,
-            Command::DecPtr => i -= 1,
-            Command::Read => {
-                let mut input = String::new();
-                io::stdin().read_line(&mut input).unwrap();
-                ctx.memory[i] = input.as_bytes()[0]
-            }
-            Command::Write => print!("{}", ctx.memory[i] as u8 as char),
-            Command::JZ(disp) => {
-                if ctx.memory[i] == 0 {
-                    ctx.pc += *disp.borrow();
-                }
-            }
-            Command::JNZ(disp) => {
-                if ctx.memory[i] != 0 {
-                    ctx.pc -= disp
-                }
-            }
-        }
-        ctx.pc += 1;
-    }
-}
-
-fn read_file(filename: &str) -> String {
-    return fs::read_to_string(filename).unwrap();
 }
